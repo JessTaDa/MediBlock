aeedd7c8c3ab8541544e66aca5e92651f972bd33
JESSTADA <jesstada@gmail.com>
2019-02-07 13:57:16 +1100

    working cloned repo

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       gas: 4712388,   // <--- Twice as much
       gasPrice: 100000000000,
       network_id: "*" // Match any network id
